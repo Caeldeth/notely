9086fcd0cd88955bb4a8c48a500269b95b179c55
Caeldeth <sabrael@hybrasyl.com>
2021-11-07 11:30:34 -0600

    save point

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,5 @@
+// initialize router
+const router = require('express').Router();
+// link to database
+const { notesDB } = require('../../db/db.json');
+// const { newNote, findByIndex, editNote, delNote } = require('../../lib/notes');
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+//initialize express
 const express = require('express');
+//link app to express
 const app = express();
+//set variable port for prod/dev
 const PORT = process.env.PORT || 3001;
 
+//link api routes
 const apiRoutes = require('./routes/apiRoutes');
+//link html routes
 const htmlRoutes = require('./routes/htmlRoutes');
 
+//receive html
+app.use(express.urlencoded({ extended: true}));
+//enable reading json
+app.use(express.json());
+//set public dir
+app.use(express.static('public'));
+
+//set api route
+app.use ('/api', apiRoutes);
+//set html route
+app.use ('/', htmlRoutes);
+
+//add confirm that port is listening
 app.listen(PORT, () => {
     console.log(`API server is active on port ${PORT}!`);
 });
